b77c285e803e6cb7f1e14d99680dfbcfa0e94143
superman312 <68647023+superman312@users.noreply.github.com>
2022-02-11 10:06:42 +0100

    path fix

--- PHP/Header.php
+++ PHP/Header.php
@@ -1,5 +1,5 @@
 <nav>
-    <a href="index.php">Home</a>
+    <a href="../index.php">Home</a>
     <a href="pages/service.php">Services</a>
     <a href="pages/voorlichting.php">Voorlichting</a>
     <a href="pages/mijnAPO.php">MijnAPO</a>
