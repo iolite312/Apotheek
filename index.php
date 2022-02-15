9d6c54bccd5ae85d0fadb0ced38080039611a8d1
superman312 <68647023+superman312@users.noreply.github.com>
2022-02-15 11:53:45 +0100

    slideshow broken

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="description" content="Huisartsenpraktijk Schut">
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/slideshow.css">
     <title>Apotheek Schut</title>
 </head>
 
@@ -16,12 +17,54 @@
         <img class="logo" src="./img/placeholderimg.jpg" alt="Logo van de apotheek">
         <?php include 'PHP/Header.php' ?>
     </header>
-    <noscript>
-        <img src="./img/placeholderimg.jpg" alt="">
-    </noscript>
-    <h2>Onze services</h2>
-    <p class="textwidth">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo nam natus iusto vel. Voluptate, distinctio sed? Accusantium ex, necessitatibus eos consequatur nesciunt eveniet veritatis iure natus voluptates nam cupiditate veniam et!</p>
+    <h2 class="newstext">Nieuws</h2>
+    <div class="news">
+        <img class="newsplaceholder" src="./img/placeholderimg.jpg" alt="news">
+        <img class="newsplaceholder" src="./img/placeholderimg.jpg" alt="news">
+        <img class="newsplaceholder" src="./img/placeholderimg.jpg" alt="news">
+    </div>
+    <script src="./js/slideshow.js" defer></script>
+    <!-- Slideshow container -->
+    <div class="slideshow-container">
 
+        <!-- Full-width images with number and caption text -->
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img class="img" src="./img/placeholderimg.jpg">
+            <div class="text">Caption Text</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img class="img" src="./img/Schermafbeelding 2022-02-10 105447.png">
+            <div class="text">Caption Two</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img class="img" src="./img/placeholderimg.jpg">
+            <div class="text">Caption Three</div>
+        </div>
+
+        <!-- Next and previous buttons -->
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
+    <br>
+
+    <!-- The dots/circles -->
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+    <div class="text2">
+        <h2>Onze services</h2>
+        <p class="textwidth">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo nam natus iusto vel. Voluptate, distinctio sed? Accusantium ex, necessitatibus eos consequatur nesciunt eveniet veritatis iure natus voluptates nam cupiditate veniam et!</p>
+    </div>
+    <footer>
+        <?php include 'PHP/Footer.php' ?>
+    </footer>
 </body>
 
 </html>
