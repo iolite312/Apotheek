f1a87453af3e914687095cb409e5b9891144e46b
superman312 <68647023+superman312@users.noreply.github.com>
2022-02-15 15:14:54 +0100

    mapping fix

--- PHP/Footer.php
+++ PHP/Footer.php
@@ -14,6 +14,6 @@
         <p class="contactt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, libero consequuntur.</p>
     </div>
     <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3802.9471680610595!2d4.7949652913015415!3d52.78510496483071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf4e258ca8ccdb%3A0x3272ca6e663a9bac!2sROC%20Kop%20Noord-Holland!5e0!3m2!1snl!2snl!4v1644928675404!5m2!1snl!2snl" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3802.9471680610595!2d4.7949652913015415!3d52.78510496483071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf4e258ca8ccdb%3A0x3272ca6e663a9bac!2sROC%20Kop%20Noord-Holland!5e0!3m2!1snl!2snl!4v1644928675404!5m2!1snl!2snl" width="250" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 </div>
