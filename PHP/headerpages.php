411718660271d6fded9e9854327fb321332de81a
superman312 <68647023+superman312@users.noreply.github.com>
2022-03-08 20:30:21 +0100

    deletos

--- PHP/headerpages.php
+++
@@ -1,6 +0,0 @@
-<nav class="navbarlocation">
-    <a class="navbarstyle <?php if($page=='index'){echo 'active';} ?>" href="../index.php">Home</a>
-    <a class="navbarstyle <?php if($page=='service'){echo 'active';} ?>" href="service.php">Services</a>
-    <a class="navbarstyle <?php if($page=='voorlichting'){echo 'active';} ?>" href="http://localhost/Apotheek/pages/voorlichting.php?sort=&submit=Verzenden">Voorlichting</a>
-    <a class="navbarstyle <?php if($page=='mijnapo'){echo 'active';} ?>" href="mijnAPO.php">MijnAPO</a>
-</nav>
