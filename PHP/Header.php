2211264834ff16411e91fc6410230af421e21755
superman312 <68647023+superman312@users.noreply.github.com>
2022-03-18 11:08:12 +0100

    htaccess

--- PHP/Header.php
+++ PHP/Header.php
@@ -1,6 +1,6 @@
 <nav class="navbarlocation">
-    <a class="navbarstyle <?php if($page == 'index'){echo 'active';} ?>" href="/apo_leon/index.php">Home</a>
-    <a class="navbarstyle <?php if($page == 'service'){echo 'active';} ?>" href="/apo_leon/pages/service.php">Services</a>
-    <a class="navbarstyle <?php if($page == 'voorlichting'){echo 'active';} ?>" href="/apo_leon/pages/voorlichting.php?sort=&submit=Verzenden">Voorlichting</a>
-    <a class="navbarstyle <?php if($page == 'mijnapo'){echo 'active';} ?>" href="/apo_leon/pages/mijnAPO.php">MijnAPO</a>
+    <a class="navbarstyle <?php if($page == 'index'){echo 'active';} ?>" href="/apo_leon/index">Home</a>
+    <a class="navbarstyle <?php if($page == 'service'){echo 'active';} ?>" href="/apo_leon/pages/service">Services</a>
+    <a class="navbarstyle <?php if($page == 'voorlichting'){echo 'active';} ?>" href="/apo_leon/pages/voorlichting?sort=&submit=Verzenden">Voorlichting</a>
+    <a class="navbarstyle <?php if($page == 'mijnapo'){echo 'active';} ?>" href="/apo_leon/pages/mijnAPO">MijnAPO</a>
 </nav>
