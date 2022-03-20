2e18062d4229cae58dd091e979b0cafb764ea5df
superman312 <68647023+superman312@users.noreply.github.com>
2022-03-20 19:47:08 +0100

    ?

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <body>
     <header>
         <img class="logo" src="./img/placeholderimg.jpg" alt="Logo van de apotheek">
-        <?php include $_SERVER["DOCUMENT_ROOT"] .  '/apo_leon/PHP/header.php' ?>
+        <?php include '/apo_leon/PHP/header.php' ?>
     </header>
     <h2 class="newstext">Nieuws</h2>
     <!-- <div class="news">
