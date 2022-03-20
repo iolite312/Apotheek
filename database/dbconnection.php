a2fd80c230e3088850405427f60f686eca585b6e
superman312 <68647023+superman312@users.noreply.github.com>
2022-03-20 18:31:27 +0100

    barf

--- database/dbconnection.php
+++ database/dbconnection.php
@@ -5,9 +5,9 @@
     $dbname = 'apotheek';
     $conn = mysqli_connect($servername,$username, $password, $dbname);
     //check that connection happend
-    if(mysqli_connect_errno())
-    {
-        echo "1: Connection Failed"; //error code #1 - connection failed
-        exit();
-    }
+    // if(mysqli_connect_errno())
+    // {
+    //     echo "1: Connection Failed"; //error code #1 - connection failed
+    //     exit();
+    // }
 ?>
